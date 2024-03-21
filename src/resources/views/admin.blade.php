94b66da163d270f70fe61861769a600522ab8cb5
H.Shimizu <hiroi.s.612@gmail.com>
2024-03-21 20:35:27 +0900

    03212

--- src/resources/views/admin.blade.php
+++ src/resources/views/admin.blade.php
@@ -2,6 +2,7 @@
 
 @section('css')
 <link rel="stylesheet" href="css/admin.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 @endsection
 
 @section('header')
